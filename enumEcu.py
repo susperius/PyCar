ffc5c0c8ecadc180c899d2b05af45e84a0a41940
susperius <susperius@susp-net.de>
2013-10-30 15:52:52 +0100

    OnBoard Enumeration and logging

--- enumEcu.py
+++ enumEcu.py
@@ -4,18 +4,19 @@ ser = obd2.ObdConnection('/dev/ttyUSB0', wait_time=0)
 
 print(ser.communicate('AT H1 \r'))
 
-x = 0
-
+x = 0x6 
+z = 0
 for a in range(0x7):
-    y = 0
-    for b in range(0xF):
-        z = 0
-        for c in range(0xF):
+    y = 1
+    for b in range(0x10):
+        z = 0 
+        for c in range(1):
             print('--------------------------------------------------')
             print('Address -> ', ser.communicate('AT SH '+hex(x)[2:4]+hex(y)[2:4]+hex(z)[2:4]+' \r'))
             print('01 -> ', ser.communicate('01 3E \r'))
             print('02 -> ', ser.communicate('02 3E \r'))
-            z += 1
+            print('03 -> ', ser.communicate('3E \r'))
+            print('04 -> ', ser.communicate('09 0A \r'))
         y += 1
     x += 1
       
